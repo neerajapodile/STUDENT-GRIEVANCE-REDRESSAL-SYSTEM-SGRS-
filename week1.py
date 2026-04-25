7b8148ab8694fada734f0506da40af8a3c638e7f
neerajapodile <neerajapodile@gmail.com>
2026-04-25 14:34:47 +0530

    Create week1.py

---
+++ week1.py
@@ -0,0 +1,18 @@
+# Student Grievance Redressal System
+# Week 1 - Basic Input Output Program
+
+print("STUDENT GRIEVANCE REDRESSAL SYSTEM")
+
+student_name = input("Enter Student Name : ")
+student_id = input("Enter Student ID : ")
+department = input("Enter Department : ")
+category = input("Enter Grievance Category : ")
+description = input("Enter Grievance Description : ")
+
+print("\n------ STUDENT GRIEVANCE DETAILS ------")
+print("Student Name        :", student_name)
+print("Student ID          :", student_id)
+print("Department          :", department)
+print("Category            :", category)
+print("Description         :", description)
+print("--------------------------------------")
